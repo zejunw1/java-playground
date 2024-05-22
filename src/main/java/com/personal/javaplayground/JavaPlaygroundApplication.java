1d045a1a3f3a5e3be5ca15f56aa9afb3375bdb12
zejunw01 <zejunw01@gmail.com>
2024-05-22 20:48:50 +0200

    Add gradle

--- src/main/java/com/personal/javaplayground/JavaPlaygroundApplication.java
+++ src/main/java/com/personal/javaplayground/JavaPlaygroundApplication.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.personal.javaplayground", "com.personal.javaplayground.controller"
         , "com.personal.javaplayground.services", "com.personal.javaplayground.daos"})
-public class JavaplaygroundApplication {
+public class JavaPlaygroundApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaplaygroundApplication.class, args);
+        SpringApplication.run(JavaPlaygroundApplication.class, args);
     }
 }
--- src/test/java/com/personal/javaplayground/JavaPlaygroundApplicationTests.java
+++ src/test/java/com/personal/javaplayground/JavaPlaygroundApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavaplaygroundApplicationTests {
+class JavaPlaygroundApplicationTests {
 
 	@Test
 	void contextLoads() {
